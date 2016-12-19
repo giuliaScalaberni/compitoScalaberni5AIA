0dc878c44ac7b6b9098d30133dc13b4d732e3868
Giulia Scalaberni <scalaberni.giulia@itismeucci.com>
2016-12-19 10:43:35 +0100

    some changes

--- src/compitoscalaberni/CompitoScalaberni.java
+++ src/compitoscalaberni/CompitoScalaberni.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package compitoscalaberni;
 
 /**
@@ -10,13 +6,10 @@ package compitoscalaberni;
  * @author Scalaberni Giulia
  */
 public class CompitoScalaberni {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int i;
         for (i=1;i<11;i++){
+            System.out.println("Puntatore: "+i);
         }
         System.out.println(i);
     }
